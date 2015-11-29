50870cf319d5d630cc2cab4fa6e4c1a7fdb4e219
shamimWP <reza@ipvision-inc.com>
2015-11-29 09:59:40 +0600

    my commit

---
+++ LLMSCustomSelectionDesign/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Phone.Controls;
+using System.Collections.ObjectModel;
+
+namespace LLMSCustomSelectionDesign
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+
+        ObservableCollection<string> llmsList;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            llmsList = new ObservableCollection<string>();
+            llmsAlbumMedia.ItemsSource = llmsList;
+            GetMediaList();
+        }
+
+        private void GetMediaList()
+        {
+            try
+            {
+                llmsList.Add("/Assets/Img/1.png");
+                llmsList.Add("/Assets/Img/2.png");
+                llmsList.Add("/Assets/Img/3.png");
+                llmsList.Add("/Assets/Img/4.png");
+                llmsList.Add("/Assets/Img/5.png");
+                llmsList.Add("/Assets/Img/6.png");
+                llmsList.Add("/Assets/Img/7.png");
+                llmsList.Add("/Assets/Img/8.png");
+            }
+            catch (Exception) { }
+        }
+    }
+}
